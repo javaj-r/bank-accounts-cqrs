6849bcd1904ffaec43159f92d3f45f72a590c9b5
javid <javid.rafiee@gmail.com>
2022-06-30 22:07:25 +0330

    Initializing project.

---
+++ src/main/java/com/javid/bank/accounts/BankAccountsCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.javid.bank.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountsCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountsCqrsApplication.class, args);
+    }
+
+}
